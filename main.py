c6ce05a06b094b62188f21676d4ed219dd70a48e
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2025-03-03 17:48:32 +0100

    add methods to fetch and process VK profile photos

--- main.py
+++ main.py
@@ -26,7 +26,80 @@ class VKAPIClient:
         self.user_id = user_id
 
 
+    def get_common_params(self) -> Dict:
+        """
+        Возвращает базовые параметры для запросов к API VK.
 
+        :return: dict - Словарь с параметрами 'access_token' и 'v' (версия API).
+        """
+        return {
+            "access_token": self.token,
+            "v": '5.199'
+        }
+
+    def _build_url(self, api_method: str) -> str:
+        """
+        Формирует URL для вызова метода API VK.
+
+        :param api_method: str - Название метода API.
+        :return: str - Полный URL запроса.
+        """
+        return f'{self.API_BASE_URL}/{api_method}'
+
+    def get_profile_photo(self) -> dict:
+        """
+        Получает фотографии профиля пользователя.
+
+        :return: dict - JSON-ответ от VK API с данными о фотографиях.
+        """
+        params = self.get_common_params()
+        params.update({
+            'owner_id': self.user_id,
+            'album_id': 'profile',
+            'rev': 1,  # Показывает фото в обратном порядке (новые первыми)
+            'extended': 1,  # Включает дополнительные поля (количество лайков)
+            'photo_sizes': 1  # Включает информацию о размерах фото
+        })
+        response = requests.get(self._build_url('photos.get'), params=params)
+        return response.json()
+
+    def get_largest_photo(self) -> List[Dict]:
+        """
+        Извлекает 5 самых больших фотографий профиля пользователя.
+
+        :return: list - Список словарей с данными о фото (лайки, URL, дата).
+        """
+        photos = self.get_profile_photo()
+
+        if 'response' in photos and 'items' in photos['response']:
+            photo_items = photos['response']['items']
+        else:
+            return []
+
+        max_size = []
+
+        for photo in photo_items:
+            sizes = photo.get('sizes', [])
+            if not sizes:
+                continue
+
+            # Находим фото с наибольшей площадью
+            max_photo_size = max(sizes, key=lambda size: size['height'] * size['width'])
+            max_url = max_photo_size['url']
+            likes = photo['likes']['count']
+            date = photo['date']
+
+            max_size.append({
+                'likes': likes,
+                'url': max_url,
+                'date': date
+            })
+
+        # Сортируем по количеству лайков (при равных лайках можно сортировать по дате)
+        sorted_max_size = sorted(max_size, key=lambda x: x['likes'], reverse=True)
+
+        # Выбираем 5 самых популярных фото
+        return sorted_max_size[:5]
 
 class YD:
     """
